aa1e1ed306d9d617882e7b8c93031e630d550732
rainwoodman <rainwoodman@1ffc4f22-0e42-0410-a188-a74355021015>
2008-01-11 19:51:50 +0000

    0.2 tag

--- gnome2-globalmenu-applet/src/misc.h
+++ gnome2-globalmenu-applet/src/misc.h
@@ -1,3 +1,2 @@
 gboolean wnck_window_is_stealable_menubar(WnckWindow * window);
-Window menubar_window_get_master(WnckWindow * wnck_menubarwindow);
-void gdk_foreign_window_send_resize_event (GdkWindow * window, gint w, gint h);
+XWindowID menubar_window_get_master(WnckWindow * wnck_menubarwindow);
