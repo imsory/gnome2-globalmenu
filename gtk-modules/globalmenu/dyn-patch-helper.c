8042c33c0b640ea2de6ccbd1940378dcd5756d59
fengshenx <fengshenx@1ffc4f22-0e42-0410-a188-a74355021015>
2008-12-12 03:03:28 +0000

    fork for 0.7-qt.

---
+++ gtk-modules/globalmenu/dyn-patch-helper.c
@@ -0,0 +1,172 @@
+#include <gtk/gtk.h>
+extern void dyn_patch_widget();
+extern void	dyn_patch_menu_shell();
+extern void	dyn_patch_menu_bar();
+/*
+ * _USE_CLOSURES doesn't help improving the performance.
+ * */
+
+static GQuark __MENUBAR__ = 0;
+static GQuark __DIRTY__ = 0;
+static GQuark __OLD_SUBMENU__ = 0;
+static GQuark __ITEM__  =  0;
+static GQuark __LABEL_NOTIFY_CLOSURE__ = 0;
+static GQuark __SUBMENU_NOTIFY_CLOSURE__ = 0;
+static gulong SIGNAL_NOTIFY = 0;
+static GQuark DETAIL_SUBMENU = 0;
+static GQuark DETAIL_LABEL = 0;
+
+static GTimer * timer = NULL;
+static gulong buffered_changes = 0;
+void dyn_patch_init () {
+	__MENUBAR__ = g_quark_from_string("__menubar__");
+	__DIRTY__ = g_quark_from_string("__dirty__");
+	__OLD_SUBMENU__ = g_quark_from_string("__old_submenu__");
+	__ITEM__ = g_quark_from_string("__item__");
+	__LABEL_NOTIFY_CLOSURE__ = g_quark_from_string("__label_notify_closure__");
+	__SUBMENU_NOTIFY_CLOSURE__ = g_quark_from_string("__submenu_notify_closure__");
+	SIGNAL_NOTIFY = g_signal_lookup("notify", G_TYPE_OBJECT);
+	DETAIL_SUBMENU = g_quark_from_string("submenu");
+	DETAIL_LABEL = g_quark_from_string("label");
+
+	dyn_patch_widget();
+	dyn_patch_menu_shell();
+	dyn_patch_menu_bar();
+	timer = g_timer_new();
+	g_timer_stop(timer);
+}
+
+static gboolean _dyn_patch_emit_changed(GtkMenuBar * menubar) {
+	g_message("Changed: %p", menubar);
+	g_object_set_qdata((GObject*)menubar, __DIRTY__, NULL);
+	g_signal_emit_by_name(menubar, "changed", 0, NULL);
+	g_message("_dyn_patch_set_menu_bar_r consumption: %lf, buffered_changes = %ld ", g_timer_elapsed(timer, NULL), buffered_changes);
+	buffered_changes = 0;
+
+	g_timer_reset(timer);
+	g_timer_stop(timer);
+	return FALSE;
+}
+void dyn_patch_queue_changed(GtkMenuBar * menubar, GtkWidget * widget) {
+	buffered_changes++;
+	if(g_object_get_qdata((GObject*)menubar, __DIRTY__)) return;
+	g_object_set_qdata((GObject*) menubar, __DIRTY__, GINT_TO_POINTER(1));
+	g_idle_add_full(G_PRIORITY_HIGH_IDLE, (GSourceFunc) _dyn_patch_emit_changed, g_object_ref(menubar), g_object_unref);
+}
+
+GtkMenuBar * dyn_patch_get_menubar(GtkWidget * widget) {
+	if(GTK_IS_MENU_BAR(widget)) return widget;
+	return g_object_get_qdata((GObject*)widget, __MENUBAR__);
+}
+void dyn_patch_set_menubar(GtkWidget * widget, GtkMenuBar * menubar) {
+	if(menubar != NULL) {
+		g_object_set_qdata_full((GObject*) widget, __MENUBAR__, g_object_ref(menubar), g_object_unref);
+	} else {
+		g_object_set_qdata((GObject*) widget, __MENUBAR__, NULL);
+	}
+}
+static void _dyn_patch_label_notify(GtkWidget * widget, GParamSpec * pspec, GtkMenuBar * menubar) {
+	dyn_patch_queue_changed(menubar, widget);
+}
+static void _dyn_patch_visible_notify(GtkWidget * widget, GParamSpec * pspec, GtkMenuBar * menubar) {
+	dyn_patch_queue_changed(menubar, widget);
+}
+static void _dyn_patch_active_notify(GtkWidget * widget, GParamSpec * pspec, GtkMenuBar * menubar) {
+	dyn_patch_queue_changed(menubar, widget);
+}
+static void _dyn_patch_inconsistent_notify(GtkWidget * widget, GParamSpec * pspec, GtkMenuBar * menubar) {
+	dyn_patch_queue_changed(menubar, widget);
+}
+static void _dyn_patch_draw_as_radio_notify(GtkWidget * widget, GParamSpec * pspec, GtkMenuBar * menubar) {
+	dyn_patch_queue_changed(menubar, widget);
+}
+static void _dyn_patch_submenu_notify(GtkWidget * widget, GParamSpec * pspec, GtkMenuBar * menubar) {
+	GtkWidget * old_submenu = g_object_get_qdata((GObject*) widget, __OLD_SUBMENU__);
+	GtkWidget * submenu = gtk_menu_item_get_submenu((GtkMenuItem*)widget);
+	g_message("submenu changed %p %p", widget, submenu);
+	if(submenu != old_submenu) {
+		if(old_submenu) {
+			dyn_patch_set_menubar_r(old_submenu, NULL);
+		}
+		if(submenu) {
+			dyn_patch_set_menubar_r(submenu, menubar);
+			g_object_set_qdata_full((GObject*) widget, __OLD_SUBMENU__, g_object_ref(submenu), g_object_unref); 
+		} else {
+			g_object_set_qdata((GObject*) widget, __OLD_SUBMENU__, NULL); 
+		}
+		/* although the property already hold a reference, 
+		 * we want to ensure old_submenu above is still alive
+		 * */
+		dyn_patch_queue_changed(menubar, widget);
+	}
+}
+
+void dyn_patch_set_menubar_r(GtkWidget * widget, GtkMenuBar * menubar) {
+	g_timer_continue(timer);
+	GtkWidget * old = (GtkWidget*) dyn_patch_get_menubar(widget);
+	if(old != menubar) {
+		if(old && GTK_IS_LABEL(widget))
+			g_signal_handlers_disconnect_by_func(widget, 
+					_dyn_patch_label_notify, 
+					menubar);
+		if(old && GTK_IS_MENU_ITEM(widget)) {
+			g_signal_handlers_disconnect_by_func(widget, 
+					_dyn_patch_submenu_notify, 
+					menubar);
+			g_signal_handlers_disconnect_by_func(widget, 
+					_dyn_patch_visible_notify, 
+					menubar);
+		}
+		if(menubar && GTK_IS_CHECK_MENU_ITEM(widget)) {
+			g_signal_handlers_disconnect_by_func(widget, 
+					_dyn_patch_draw_as_radio_notify, 
+					menubar);
+			g_signal_handlers_disconnect_by_func(widget, 
+					_dyn_patch_inconsistent_notify, 
+					menubar);
+			g_signal_handlers_disconnect_by_func(widget, 
+					_dyn_patch_active_notify, 
+					menubar);
+		}
+	}
+	g_timer_stop(timer);
+	dyn_patch_set_menubar(widget, menubar);
+
+	if(GTK_IS_CONTAINER(widget)) {
+		GList * children = gtk_container_get_children((GtkContainer*)widget);
+		GList * node;
+		for(node = children; node; node = node->next) {
+			dyn_patch_set_menubar_r(node->data, menubar);
+		}
+	}
+	if(GTK_IS_MENU_ITEM(widget)) {
+		GtkWidget * submenu = gtk_menu_item_get_submenu((GtkMenuItem*)widget);
+		if(submenu) {
+			g_object_set_qdata_full((GObject*) submenu, __ITEM__, g_object_ref(widget), g_object_unref);
+			dyn_patch_set_menubar_r(submenu, menubar);
+		}
+	}
+	g_timer_continue(timer);
+	if(menubar != old) {
+		if(menubar && GTK_IS_LABEL(widget)) {
+			g_signal_connect(widget, "notify::label", 
+					_dyn_patch_label_notify, menubar);
+		}
+		if(menubar && GTK_IS_MENU_ITEM(widget)) {
+			g_signal_connect(widget, "notify::submenu", 
+					_dyn_patch_submenu_notify, menubar);
+			g_signal_connect(widget, "notify::visible", 
+					_dyn_patch_visible_notify, menubar);
+		}
+		if(menubar && GTK_IS_CHECK_MENU_ITEM(widget)) {
+			g_signal_connect(widget, "notify::active", 
+					_dyn_patch_active_notify, menubar);
+			g_signal_connect(widget, "notify::inconsistent", 
+					_dyn_patch_inconsistent_notify, menubar);
+			g_signal_connect(widget, "notify::draw-as-radio", 
+					_dyn_patch_draw_as_radio_notify, menubar);
+		}
+	}
+	g_timer_stop(timer);
+}
+
---
+++ gtk-modules/globalmenu/globalmenu.vala
@@ -0,0 +1,114 @@
+using Gtk;
+
+namespace GnomenuGtk {
+
+	[CCode (cname = "gdk_window_get_desktop_hint")]
+	protected extern bool gdk_window_get_is_desktop (Gdk.Window window);
+	[CCode (cname = "gtk_widget_get_toplevel_window")]
+	protected extern weak Gtk.Window? gtk_widget_get_toplevel_window (Gtk.Widget widget);
+	[CCode (cname = "gdk_window_set_menu_context")]
+	protected extern void gdk_window_set_menu_context (Gdk.Window window, string context);
+	[CCode (cname = "gdk_window_get_menu_event")]
+	protected extern string gdk_window_get_menu_event (Gdk.Window window);
+
+	protected void add_emission_hooks() {
+		uint signal_id = Signal.lookup("changed", typeof(MenuBar));
+		Signal.add_emission_hook (signal_id, 0, changed_eh, null);
+		uint signal_id_hc = Signal.lookup("hierarchy-changed", typeof(Gtk.Widget));
+		Signal.add_emission_hook (signal_id_hc, 0, hierachy_changed_eh, null);
+	}
+
+	private bool changed_eh (SignalInvocationHint ihint, 
+			[CCode (array_length_pos = 1.9) ]
+			Value[] param_values
+	) {
+		MenuBar self = param_values[0].get_object() as MenuBar;
+		if(self != null) {
+			if(ihint.run_type != SignalFlags.RUN_FIRST) return true;
+			Gtk.Window toplevel = self.get_ancestor(typeof(Gtk.Window)) as Gtk.Window;
+			if(toplevel != null && (0 != (toplevel.get_flags() & WidgetFlags.REALIZED))) {
+				gdk_window_set_menu_context(toplevel.window, 
+						Serializer.to_string(self)
+						);
+			}
+		} 
+		return true;
+	}
+	private void bonobo_plug_widget_hack(Gtk.Widget self) {
+		weak Gtk.Widget parent = self.parent;
+		while(parent is Gtk.Widget) {
+			weak string typename = parent.get_type().name();
+			if(typename.str("Bonobo")!= null) {
+				message("Hiding %s", typename);
+				parent.hide();
+			}
+			parent = parent.parent;
+		} 
+
+	}
+	private bool hierachy_changed_eh (SignalInvocationHint ihint, 
+			[CCode (array_length_pos = 1.9) ]
+			Value[] param_values
+	) {
+
+		MenuBar self = param_values[0].get_object() as MenuBar;
+		if(self == null) return true;
+		Gtk.Widget old_toplevel = param_values[1].get_object() as Gtk.Widget;
+		
+		Gtk.Window old_toplevel_window = gtk_widget_get_toplevel_window(old_toplevel);
+		Gtk.Window toplevel_window = gtk_widget_get_toplevel_window(self);
+
+		bonobo_plug_widget_hack(self);
+		if(old_toplevel_window != null) {
+			old_toplevel_window.property_notify_event -= window_property_notify_event;
+			old_toplevel_window.set_data("__menubar__", null);
+			/*
+			uint source_id = (uint) old_toplevel_window.get_data("__keep_alive__");
+			Source.remove(source_id);
+			*/
+		}
+		if(toplevel_window != null) {
+			toplevel_window.set_data_full("__menubar__", self.ref(), g_object_unref);
+			toplevel_window.add_events(Gdk.EventMask.PROPERTY_CHANGE_MASK);
+			toplevel_window.property_notify_event += window_property_notify_event;
+			/*
+			uint source_id = g_timeout_add_full(Priority.DEFAULT, 1000,
+					(data) => {
+						weak Gtk.Window window = data as Window;
+						if(window.ref_count == 1) {
+							message("The window is destroyed, no long need to keep an eye on PROPERTY_CHANGE_MASK. ");
+							return false;
+						}
+						if((window.get_events() 
+							& Gdk.EventMask.PROPERTY_CHANGE_MASK) == 0) {
+							message("Need to reset the event mask");
+						//	window.add_events(Gdk.EventMask.PROPERTY_CHANGE_MASK);
+						}
+						return true;
+					}, toplevel_window.ref(), g_object_unref);
+			toplevel_window.set_data("__keep_alive__", (void*) source_id);
+			*/
+	  	} 
+		return true;
+	}
+
+	private bool window_property_notify_event (Window window, Gdk.EventProperty event) {
+		if(event.atom == Gdk.Atom.intern("_NET_GLOBALMENU_MENU_EVENT", false)) {
+			string path = gdk_window_get_menu_event(window.window);
+			MenuBar menubar = window.get_data("__menubar__") as MenuBar;
+			message("path = %s", path);
+			if(menubar != null) {
+				MenuItem item = Locator.locate(menubar, path);
+				if(item != null) {
+					item.activate();
+				} else {
+					message("item lookup failure");
+				}
+			} else {
+				message("menubar lookup failure");
+			}
+		}
+		return false;
+	}
+
+}
